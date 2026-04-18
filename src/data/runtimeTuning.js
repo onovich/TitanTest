4ac3059467aeb5807bbef68efd8026a104b3cd57
onovich <hjn1110@163.com>
2026-04-19 05:25:03 +0800

    chore: autotune iteration 012

--- src/data/runtimeTuning.js
+++ src/data/runtimeTuning.js
@@ -13,8 +13,8 @@ export const RUNTIME_TUNING = {
     "fatalism": 0.13,
     "cause": 0.31499999999999995
   },
-  "matchDensityPenaltyWeight": 2.7000000000000006,
+  "matchDensityPenaltyWeight": 2.920000000000001,
   "neutralCenterPenaltyWeight": 2.35,
-  "ambiguityCenter": 10.960000000000006,
-  "ambiguityWidth": 6.240000000000001
+  "ambiguityCenter": 11.110000000000007,
+  "ambiguityWidth": 6.360000000000001
 };
