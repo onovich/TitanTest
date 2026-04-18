9eeaa6da7446120322ab1299bdab36c815e6d9d4
onovich <hjn1110@163.com>
2026-04-19 05:25:30 +0800

    chore: autotune iteration 017

--- src/data/runtimeTuning.js
+++ src/data/runtimeTuning.js
@@ -9,12 +9,12 @@ export const RUNTIME_TUNING = {
   "optionScoreCalibrationStrengths": {
     "freedom": 0.11000000000000001,
     "moral": 0.14,
-    "realism": 0.47,
+    "realism": 0.44999999999999996,
     "fatalism": 0.12000000000000001,
-    "cause": 0.30999999999999994
+    "cause": 0.2899999999999999
   },
-  "matchDensityPenaltyWeight": 2.920000000000001,
+  "matchDensityPenaltyWeight": 2.7400000000000007,
   "neutralCenterPenaltyWeight": 2.35,
-  "ambiguityCenter": 11.150000000000007,
+  "ambiguityCenter": 11.030000000000008,
   "ambiguityWidth": 6.480000000000001
 };
